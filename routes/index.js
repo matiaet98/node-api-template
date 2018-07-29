66f7eb59bdeb66d8750ef0469b697109502d7a4f
matiaet98 <utn.matias@gmail.com>
2018-07-29 02:47:25 -0300

    mejora funcion de ejemplo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var database = require('../database');
-var oracledb = require('oracledb');
-var numRows = 1; // rows que devuelve el fetch al cursor, 0 para ilimitado
-
+var database = require("../database");
+var oracledb = require("oracledb");
 
 /*
 create or replace PROCEDURE GET_TEST(I_ID IN NUMBER,O_CURSOR OUT SYS_REFCURSOR)
@@ -16,7 +14,11 @@ BEGIN
 END;
 */
 
-router.get('/', function (req, res, next) {
+router.get("/", function (req, res, next) {
+  if (!req.query.id) {
+    res.status(404).send("Debe enviar el ID por query string");
+    return;
+  }
   database.handleConnection(req, res, function (req, res, conn) {
     var bindvars = {
       id: req.query.id,
@@ -24,37 +26,37 @@ router.get('/', function (req, res, next) {
         type: oracledb.CURSOR,
         dir: oracledb.BIND_OUT
       }
-    }
-    conn.execute(
-      "BEGIN ORA.GET_TEST(:id, :cursor); END;",
-      bindvars,
-      function (err, results) {
-        var allRows = [];
+    };
+    conn.execute("BEGIN ORA.GET_TEST(:id, :cursor); END;", bindvars, function (
+      err,
+      results
+    ) {
+      var allRows = [];
 
-        if (err) throw err;
+      if (err) throw err;
 
-        function processResultSet() {
-          results.outBinds.cursor.getRows(10000, function (err, rows) {
-            if (err) throw err;
+      function processResultSet() {
+        results.outBinds.cursor.getRows(10000, function (err, rows) {
+          if (err) throw err;
 
-            if (rows.length) {
-              rows.forEach(function (row) {
-                newRow = `{`;
-                results.outBinds.cursor.metaData.forEach(function (el, ind) {
-                  newRow += `"` + el.name + `":"` + row[ind] + `",`;
-                });
-                newRow = newRow.slice(0, -1) + `}`;
-                allRows.push(JSON.parse(newRow));
+          if (rows.length) {
+            rows.forEach(function (row) {
+              newRow = `{`;
+              results.outBinds.cursor.metaData.forEach(function (el, ind) {
+                newRow += `"` + el.name + `":"` + row[ind] + `",`;
               });
-              processResultSet();
-              return;
-            }
-            database.release(conn);
-            res.send(JSON.parse(JSON.stringify(allRows)));
-          });
-        }
-        processResultSet();
-      });
+              newRow = newRow.slice(0, -1) + `}`;
+              allRows.push(JSON.parse(newRow));
+            });
+            processResultSet();
+            return;
+          }
+          database.release(conn);
+          res.send(JSON.parse(JSON.stringify(allRows)));
+        });
+      }
+      processResultSet();
+    });
   });
 });
 
